624263fde9a2e8b0d88a823e0feae29620b673b0
Darrin Cherry <darrinlc@Gmail.com>
2015-05-12 23:32:34 -0500

    Assembly Commit

--- Cactus-pageobject/Cactus/Properties/AssemblyInfo.cs
+++ Cactus-pageobject/Cactus/Properties/AssemblyInfo.cs
@@ -6,9 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Cactus")]
+[assembly: AssemblyDescription("PageObject Style Framework On top of Selenium WebDriver")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Cactus")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,4 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
