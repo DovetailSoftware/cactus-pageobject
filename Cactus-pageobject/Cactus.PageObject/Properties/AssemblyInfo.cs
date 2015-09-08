4effb4f1ccf58a7ee01ab65d9b2c792f168f5f10
Chad Myers <chad.myers@gmail.com>
2015-09-08 15:57:13 -0500

    Had to rename Cactus to Cactus.PageObject to make nuget.org happy
    There was already another nuget named Cactus that is unrelated

--- Cactus-pageobject/Cactus.PageObject/Properties/AssemblyInfo.cs
+++ Cactus-pageobject/Cactus.PageObject/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cactus")]
+[assembly: AssemblyTitle("Cactus.PageObject")]
 [assembly: AssemblyDescription("PageObject Style Framework On top of Selenium WebDriver")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dovetail Software")]
-[assembly: AssemblyProduct("Cactus")]
+[assembly: AssemblyProduct("Cactus.PageObject")]
 [assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
